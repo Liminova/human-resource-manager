80c632cea3dcb5fd3804512f9828d05af96d9a2a
j1nxie <rylieeeeexd@gmail.com>
2023-03-23 14:57:08 +0700

    feat: implement Person class

---
+++ test/__init__.py
@@ -0,0 +1,3 @@
+from .test_person import TestPerson
+
+__all__ = ["TestPerson"]
---
+++ test/test_person.py
@@ -0,0 +1,20 @@
+from models import Person
+import unittest
+
+class TestPerson(unittest.TestCase):
+    def test_create_person(self):
+        person = Person("Rylie", "2003-08-22", "727", "0123456727", "Sleep")
+        self.assertEqual(person.name, "Rylie")
+        self.assertEqual(person.dob, "2003-08-22")
+        self.assertEqual(person.id, "727")
+        self.assertEqual(person.phone, "0123456727")
+        self.assertEqual(person.department, "Sleep")
+
+    def test_mutate_person(self):
+        person = Person("Rylie", "2003-08-22", "727", "0123456727", "Sleep")
+        self.assertEqual(person.id, "727")
+        person.id = "420"
+        self.assertEqual(person.id, "420")
+
+if __name__ == "__main__":
+    unittest.main()
