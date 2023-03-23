8284d5f9421ddf7fb12cd701ea3c4244f74ea9b4
j1nxie <rylieeeeexd@gmail.com>
2023-03-23 16:10:37 +0700

    style: sort imports and glob exports alphabetically

--- test/__init__.py
+++ test/__init__.py
@@ -1,4 +1,5 @@
-from .test_person import TestPerson
+from .test_company import TestCompany
 from .test_department import TestDepartment
+from .test_person import TestPerson
 
-__all__ = ["TestPerson", "TestDepartment"]
+__all__ = ["TestCompany", "TestDepartment", "TestPerson"]
