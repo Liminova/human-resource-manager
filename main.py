335d9e40385109d635fcd6d5bb5a23a043117da5
j1nxie <rylieeeeexd@gmail.com>
2023-03-26 01:39:37 +0700

    feat(main.py): handle keyboard interrupts

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import sys
+import os
+
 def main():
     print("Hello World!")
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        try:
+            sys.exit(130)
+        except SystemExit:
+            os._exit(130)
