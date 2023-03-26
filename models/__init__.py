4b18be0f6cbe2e4930db53afe47afdd3466787c4
j1nxie <rylieeeeexd@gmail.com>
2023-03-26 23:11:52 +0700

    fix(models): add missing exports

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,8 @@
+from .admin import Admin
 from .benefits import BenefitPlan
 from .company import Company
 from .department import Department
 from .employee import Employee
+from .payroll import Payroll
 
-__all__ = ["BenefitPlan", "Company", "Department", "Employee"]
+__all__ = ["Admin", "BenefitPlan", "Company", "Department", "Employee", "Payroll"]
