4690f13b5c32971a14956f16a8ba2f02cd77f336
j1nxie <rylieeeeexd@gmail.com>
2023-03-26 23:08:56 +0700

    refactor(Payroll): strip month and year

--- models/payroll.py
+++ models/payroll.py
@@ -57,32 +57,7 @@ class Payroll:          # tính lương theo tháng của mỗi nhân viên
         self.__total = self.__salary + self.__bonus - self.__tax - self.__punish
         return self
 
-    def set_month(self) -> None:
-        while True:
-            month = int(input("Enter month: "))
-            if month >= 1 and month <= 12:
-                self.__month = month
-                break
-            else:
-                print("Invalid month! Enter again!")
-
-    def get_month(self) -> int:
-        return self.__month
-
-    def set_year(self) -> None:
-        while True:
-            year = int(input("Enter year: "))
-            if year >= 2000:
-                self.__year = year
-                break
-            else:
-                print("Invalid year! Enter again!")
-
-    def get_year(self) -> int:
-        return self.__year
-
     def display(self) -> None:
-        print(f"Month: {self.__month}")
         print(f"Salary: {self.__salary}")
         print(f"Bonus: {self.__bonus}")
         print(f"Tax: {self.__tax}")
