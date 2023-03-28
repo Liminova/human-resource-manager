b4d86549674f4d47093decd58c608ff6983e6607
j1nxie <rylieeeeexd@gmail.com>
2023-03-28 21:47:41 +0700

    test: rewrite tests according to new refactors

--- test/test_company.py
+++ test/test_company.py
@@ -1,19 +1,24 @@
-from models import Company, Department, Employee, Payroll
+from models import Company, Department, Employee
 import unittest
 
 class TestCompany(unittest.TestCase):
     def test_create_company(self):
-        company = Company("Doofenshmirtz Evil Inc.", [], [])
+        company = Company()
+        company.name = "Doofenshmirtz Evil Inc."
 
         self.assertEqual(company.name, "Doofenshmirtz Evil Inc.")
 
     # FIXME: for some reason this is failing while mutating departments is fine.
     def test_mutate_company(self):
-        company = Company("Doofenshmirtz Evil Inc.", [], [])
-        payroll = Payroll(5000, 500)
-        sleep = Department("Sleep", "SLP", [])
-        rylie = Employee("Rylie", "2003-08-22", "727", "0123456727", sleep, [], payroll)
-        sleep.members.append(rylie)
+        company = Company()
+        company.name = "Doofenshmirtz Evil Inc."
+
+        sleep = Department()
+        sleep.name = "Sleep"
+        sleep.id = "SLP"
+
+        rylie = Employee() \
+            .set_name("Rylie").unwrap() \
 
         company.departments.append(sleep)
         company.employees.append(rylie)
@@ -23,8 +28,8 @@ class TestCompany(unittest.TestCase):
         self.assertEqual(company.employees, [rylie])
 
     def test_multiple_company(self):
-        company_1 = Company("Company 1", [], [])
-        company_2 = Company("Company 2", [], [])
+        company_1 = Company()
+        company_2 = Company()
 
         # this should return the first instance of Company initialized
-        self.assertEqual(company_1.name, company_2.name)
+        self.assertEqual(company_1, company_2)
