600461d14d069731097ca7bd38691dc9c1842e21
Delnegend <kiennguyen19323@gmail.com>
2023-03-30 23:24:27 +0700

    fix(Attendance): re-implement

--- models/attendance_check.py
+++ models/attendance_check.py
@@ -1,56 +1,85 @@
-import datetime as dt
 import sys
 from option import Result, Ok, Err
+from datetime import datetime
 
 if sys.version_info >= (3, 11):
     from typing import Self
 else:
     from typing_extensions import Self
 
-DEFAULT_LEAVE_BALANCE = 3
-
 class Attendance:
     def __init__(self):
-        self.__start_date = dt.datetime.now()
-        self.__leave_balance = DEFAULT_LEAVE_BALANCE
-        self.__attendance = {}
-        self.__time_off = {}
+        # date of first attendance
+        self.__start_date = datetime.now()
+        # number of days the employee absent, they're allowed to be absent 3 days per year before taking a pay cut
+        self.__allowed_absence_days = []
+        # list of tuples (date, isPresent)
+        self.__attendance = []
+        # list of tuples (date, reason)
+        self.__absences = []
 
     @property
-    def leave_balance(self) -> int:
-        return self.__leave_balance
+    def allowed_absence_days(self) -> int:
+        return self.__max_allowed_absence_days
 
     @property
-    def start_date(self) -> dt.datetime:
+    def start_date(self) -> datetime:
         return self.__start_date
 
-    def set_leave_balance(self, leave_balance: int = 0) -> Result[Self, str]:
-        self.__leave_balance = leave_balance
-        return Ok(self) if leave_balance else Err("Leave balance cannot be empty.")
+    @property
+    def attendance(self) -> list:
+        return self.__attendance
 
-    def set_start_date(self, start_date: dt.datetime = None) -> Result[Self, str]:
-        self.__start_date = start_date
-        return Ok(self) if start_date else Err("Start date cannot be empty.")
+    @property
+    def absences(self) -> list:
+        return self.__absences
 
-    # NOTE: do we really need all the abstractions below?
-
-    # NOTE: true = present, false = absent
-    def add_attendance(self, date: dt.datetime, status: bool) -> Self:
-        if date is None:
-            return Err("Date or status cannot be empty.")
-        self.__attendance[date] = status
-        return Ok(self)
-
-    # TODO: what is type?
-    # add a type hint for type here later. - Rylie
-    def add_time_off(self, date: dt.datetime, type):
-        self.__time_off[type].append(date)
-
-    def get_attendance(self, date: dt.datetime) -> Result[bool, str]:
-        return Ok(self.__attendance[date]) if date in self.__attendance else Err("Date not found.")
-
-    def get_time_off(self, date: dt.datetime) -> Result[str, str]:
-        for type in self.__time_off:
-            if date in self.__time_off[type]:
-                return Ok(type)
+    def get_attendance(self, date: str) -> Result[bool, str]:
+        if date == None:
+            return Err("Date cannot be empty.")
+        for d, isPresent in self.__attendance:
+            if d == date:
+                return Ok(isPresent)
         return Err("Date not found.")
+
+    def get_absence_reason(self, date: str) -> Result[str, str]:
+        if date == None:
+            return Err("Date cannot be empty.")
+        for d, reason in self.__absences:
+            if d == date:
+                return Ok(reason)
+        return Err("Date not found.")
+
+    def get_allowed_absence_days(self, year: int) -> Result[int, str]:
+        for y, days in self.__allowed_absence_days:
+            if y == year:
+                return Ok(days)
+        return Err("Year not found.")
+
+    def set_start_date(self, start_date: datetime = None) -> Result[None, str]:
+        self.__start_date = start_date
+        return Ok(None) if start_date else Err("Start date cannot be empty.")
+
+    def add_attendance(self, date: str, is_presence: bool) -> Result[None, str]:
+        # Check the allowed absence days first, if it doesn't contain current year, add it and set to 3
+        for year, _ in self.__allowed_absence_days:
+            if year == date.year:
+                break
+        else:
+            self.__allowed_absence_days.append((date.year, 3))
+
+        try:
+            date = datetime.strptime(date, "%Y-%m-%d")
+        except:
+            return Err("Date format is incorrect.")
+        if is_presence == None:
+            return Err("Presence status cannot be empty.")
+        self.__attendance[date] = is_presence
+        return Ok(None)
+
+    def add_absence_day(self, date: datetime, reason: str) -> Result[None, str]:
+        if date == None or reason == None:
+            return Err("Date or reason cannot be empty.")
+        self.__absences[date] = reason
+        self.__allowed_absence_days[date.year] -= 1
+        return Ok(None)
