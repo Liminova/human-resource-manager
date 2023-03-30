3eff0ebfdb4dcc5491dedd052c159184cbde0fbc
Delnegend <kiennguyen19323@gmail.com>
2023-03-30 12:22:57 +0700

    refactor(Payroll): return w/ conditional expression

--- models/payroll.py
+++ models/payroll.py
@@ -37,37 +37,24 @@ class Payroll:
         return self.__total
 
     def set_salary(self, salary: int) -> Result[Self, str]:
-        if not self.verify(salary):
-            return Err("Salary cannot be negative.")
         self.__salary = salary
         self.calculate_total()
-        return Ok(self)
+        return Ok(self) if salary >= 0 else Err("Salary cannot be negative.")
 
     def set_bonus(self, bonus: int) -> Result[Self, str]:
-        if not self.verify(bonus):
-            return Err("Bonus cannot be negative.")
         self.__bonus = bonus
         self.calculate_total()
-        return Ok(self)
+        return Ok(self) if bonus >= 0 else Err("Bonus cannot be negative.")
 
     def set_tax(self, tax: int) -> Result[Self, str]:
-        if not self.verify(tax):
-            return Err("Tax cannot be negative.")
         self.__tax = tax
         self.calculate_total()
-        return Ok(self)
+        return Ok(self) if tax >= 0 else Err("Tax cannot be negative.")
 
     def set_punish(self, punish: int) -> Result[Self, str]:
-        if not self.verify(punish):
-            return Err("Punishment cannot be negative.")
         self.__punish = punish
         self.calculate_total()
-        return Ok(self)
-
-    def verify(self, value: int) -> bool:
-        if value < 0:
-            return False
-        return True
+        return Ok(self) if punish >= 0 else Err("Punish cannot be negative.")
 
     def calculate_total(self) -> Self:
         self.__total = self.__salary + self.__bonus - self.__tax - self.__punish
