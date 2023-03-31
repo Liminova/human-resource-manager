c95c4ddb2b6467b63b4813c81fc368597c4161e3
j1nxie <rylieeeeexd@gmail.com>
2023-03-31 19:17:36 +0700

    feat(Employee): refactor into pydantic model

--- models/employee.py
+++ models/employee.py
@@ -4,6 +4,7 @@ import sys
 import textwrap
 from datetime import datetime
 from option import Result, Ok, Err
+from pydantic import BaseModel
 
 if sys.version_info >= (3, 11):
     from typing import Self
@@ -19,65 +20,24 @@ from .performance import Performance
 # NOTE: possible abstraction: split name and id into its own Entity class or
 # something, though i don't like that approach very much tbh - Rylie
 
-class Employee:
-    def __init__(self) -> None:
-        self.__name = ""
-        self.__dob: datetime = None
-        self.__email = ""
-        self.__id = ""
-        self.__phone = ""
-        self.__department = None
-        self.__benefits = []
-        self.__payroll = None
-        self.__attendance = Attendance() # inside contains lists of attendance
-        self.__performance = Performance() # inside contains lists of sales
-
-    @property
-    def name(self) -> str:
-        return self.__name
-
-    @property
-    def dob(self) -> datetime:
-        return self.__dob
-
-    @property
-    def email(self) -> str:
-        return self.__email
-
-    @property
-    def id(self) -> str:
-        return self.__id
-
-    @property
-    def phone(self) -> str:
-        return self.__phone
-
-    @property
-    def department(self) -> Department | None:
-        return self.__department
-
-    @property
-    def benefits(self) -> list[BenefitPlan]:
-        return self.__benefits
-
-    @property
-    def payroll(self) -> Payroll | None:
-        return self.__payroll
-
-    @property
-    def attendance(self) -> Attendance:
-        return self.__attendance
-
-    @property
-    def performance(self) -> Performance:
-        return self.__performance
+class Employee(BaseModel):
+    name = ""
+    dob: datetime | None = None
+    email = ""
+    id = ""
+    phone = ""
+    department: Department | None = None
+    benefits: list[BenefitPlan] = []
+    payroll: Payroll | None = None
+    attendance: Attendance = Attendance()
+    performance: Performance = Performance()
 
     def set_name(self, name: str) -> Result[Self, str]:
         if name == "":
             return Err("Name cannot be empty!")
         if any(char.isdigit() for char in name):
             return Err("Name cannot contain numbers!")
-        self.__name = name
+        self.name = name
         return Ok(self)
 
     def set_dob(self, dob: str) -> Result[Self, str]:
@@ -85,36 +45,36 @@ class Employee:
             dob = datetime.strptime(dob, "%Y-%m-%d")
         except ValueError:
             return Err("Invalid date of birth format!")
-        self.__dob = dob
+        self.dob = dob
         return Ok(self)
 
     def set_email(self, email: str = "") -> Result[Self, str]:
         pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
         if not re.match(pattern, email):
             return Err("Invalid email format!")
-        self.__email = email
+        self.email = email
         return Ok(self) if email else Err("Email cannot be empty!")
 
     def set_id(self, id: str = "") -> Result[Self, str]:
-        self.__id = id
+        self.id = id
         return Ok(self) if id else Err("ID cannot be empty!")
 
     def set_phone(self, phone: str = "") -> Result[Self, str]:
         if any(char.isalpha() for char in phone):
             return Err("Phone number cannot contain letters!")
-        self.__phone = phone
+        self.phone = phone
         return Ok(self) if phone else Err("Phone number cannot be empty!")
 
     def set_department(self, department: Department) -> Result[Self, str]:
-        self.__department = department
+        self.department = department
         return Ok(self)
 
     def set_payroll(self, payroll: Payroll) -> Result[Self, str]:
-        self.__payroll = payroll
+        self.payroll = payroll
         return Ok(self)
 
     def set_performance(self, performance: Performance) -> Result[Self, str]:
-        self.__performance = performance
+        self.performance = performance
         return Ok(self)
 
     def is_enrolled_in_plan(self, benefit: BenefitPlan) -> bool:
@@ -139,3 +99,6 @@ class Employee:
         for (i, benefit) in enumerate(self.__benefits, 1):
             data += f"{i}. {benefit.name}\n"
         return data
+
+    class Config:
+        arbitrary_types_allowed = True
