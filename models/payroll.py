b16caff8a771aab51967d9b80258564766040d85
j1nxie <rylieeeeexd@gmail.com>
2023-04-03 11:05:56 +0700

    fix(Payroll): convert to integer inside of method

--- models/payroll.py
+++ models/payroll.py
@@ -15,29 +15,33 @@ class Payroll:
     punish = 0
     total = 0
 
-    def set_salary(self, salary: int) -> Result[Self, str]:
+    def set_salary(self, salary: str) -> Result[Self, str]:
+        salary = int(salary)
         self.salary = salary
-        self.calculate_total()
+        self.calculate_total().unwrap()
         return Ok(self) if salary >= 0 else Err("Salary cannot be negative.")
 
-    def set_bonus(self, bonus: int) -> Result[Self, str]:
+    def set_bonus(self, bonus: str) -> Result[Self, str]:
+        bonus = int(bonus)
         self.bonus = bonus
-        self.calculate_total()
+        self.calculate_total().unwrap()
         return Ok(self) if bonus >= 0 else Err("Bonus cannot be negative.")
 
-    def set_tax(self, tax: int) -> Result[Self, str]:
+    def set_tax(self, tax: str) -> Result[Self, str]:
+        tax = int(tax)
         self.tax = tax
-        self.calculate_total()
+        self.calculate_total().unwrap()
         return Ok(self) if tax >= 0 else Err("Tax cannot be negative.")
 
-    def set_punish(self, punish: int) -> Result[Self, str]:
+    def set_punish(self, punish: str) -> Result[Self, str]:
+        punish = int(punish)
         self.punish = punish
-        self.calculate_total()
+        self.calculate_total().unwrap()
         return Ok(self) if punish >= 0 else Err("Punish cannot be negative.")
 
-    def calculate_total(self) -> None:
+    def calculate_total(self) -> Result[Self, str]:
         self.total = self.salary + self.bonus - self.tax - self.punish
-        return None
+        return Ok(self)
 
     def __str__(self) -> str:
         self.calculate_total()
