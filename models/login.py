865126acbe145e2991e2787bc275a6a74bd56d61
Dao Tuyet Ngan <ngandt.bi12-311@st.usth.edu.vn>
2023-04-06 15:45:31 +0700

    fix(login): fixing statement errors and incorporate hashlib
    remove some redundant statements and changed return None to return Ok(self)

--- models/login.py
+++ models/login.py
@@ -1,9 +1,11 @@
+import hashlib
+
 # example class for employee (i.e. consider adding this to models/employee.py)
 class Employee:
     """Employee class for login system."""
     def __init__(self, username: str, password: str, role: str) -> None:
         self.username = username
-        self.password = password
+        self.password = hashlib.sha256(password.encode()).hexdigest()
         self.role = role
         return None
 
@@ -21,23 +23,23 @@ class Admin(Employee):
 class Login:
     """Login system for employees and managers."""
     def __init__(self) -> None:
-        self.employees = []
-        self.admins = []
-        return None
+        self.employees: list[Employee] = []
+        self.admins: list[Admin] = []
 
     def add_employee(self, employee: Employee) -> None:
         self.employees.append(employee)
-        return None
+        return Ok(self)
 
     def add_admin(self, admin: Admin) -> None:
         self.admins.append(admin)
-        return None
+        return Ok(self)
 
-    def login(self, username: str, password: str) -> Result[Employee | Admin, str]:
+    def login(self, username: str, password: str) -> Result[Union[Employee, Admin], str]:
+        hashed_password = hashlib.sha256(password.encode()).hexdigest()
         for employee in self.employees:
-            if employee.username == username and employee.password == password:
+            if employee.username == username and employee.password == hashed_password:
                 return Ok(employee)
         for admin in self.admins:
-            if admin.username == username and admin.password == password:
+            if admin.username == username and admin.password == hashed_password:
                 return Ok(admin)
         return Err("Invalid username or password.")
