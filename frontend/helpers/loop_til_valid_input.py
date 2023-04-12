5d6bf2023017ce20421597c6a573c77be2970b71
Delnegend <kiennguyen19323@gmail.com>
2023-04-13 03:19:28 +0700

    style(loop_til_valid_input): confirm to go back

--- frontend/helpers/loop_til_valid_input.py
+++ frontend/helpers/loop_til_valid_input.py
@@ -3,7 +3,10 @@ def loop_til_valid_input(prompt: str, validator: callable) -> str: # type: ignor
     """Takes a prompt and a validator function and keeps asking for input until the validator returns True"""
     user_input = input(f"{prompt}, leave blank to cancel: ")
     if user_input == "":
-        return "Input cancelled!"
+        confirm = input("Are you sure you want to cancel? (Y/n): ")
+        if confirm.lower() != "n":
+            return "Input cancelled!"
+        user_input = input(f"{prompt}, leave blank to cancel: ")
     while True:
         try:
             validator(user_input).unwrap() # type: ignore
