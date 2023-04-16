a2b493a26c8f3f0aef7a38e4bede6cd9976b6c71
j1nxie <rylieeeeexd@gmail.com>
2023-04-17 06:53:51 +0700

    chore: ignore type error

--- frontend/helpers/get_user_option_from_list.py
+++ frontend/helpers/get_user_option_from_list.py
@@ -3,7 +3,7 @@ from .clrscr import clrscr
 from .clustering import clustering
 
 
-def get_user_option_from_list(title: str, options: list[str]) -> int:
+def get_user_option_from_list(title: str, options: list[str]) -> int: # type: ignore
     """Takes a list of options and returns the users choice's index | returns -1 if options empty, -2 if user cancels"""
 
     if len(options) == 0:
