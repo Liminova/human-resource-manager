7269b5be1ad9992a14bf3dd40aaf4b4b729b5ceb
Delnegend <kiennguyen19323@gmail.com>
2023-04-24 21:27:07 +0700

    fix(GUI/global_styling): rm input box width

--- frontend/helpers_gui/global_styling.py
+++ frontend/helpers_gui/global_styling.py
@@ -7,4 +7,4 @@ btn_menu_padding = dict(padx=20, pady=10)
 label_title_style = dict(font=("Century Gothic", 30, "bold"))
 label_desc_style = dict(font=("Century Gothic", 20, "italic"))
 
-input_box_style = dict(width=400, height=30, font=("Century Gothic", 14), corner_radius=10)
+input_box_style = dict(height=30, font=("Century Gothic", 14), corner_radius=10)
