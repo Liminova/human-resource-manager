8cf9ae9070e2c1fa2ba33195c9ef6e241336e3f0
Delnegend <kiennguyen19323@gmail.com>
2023-04-25 14:31:25 +0700

    feat(GUI display_list): add docs, more optional params

--- frontend/helpers_gui/display_list.py
+++ frontend/helpers_gui/display_list.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import customtkinter as ctk
 from frontend.helpers_tui import clustering
 from frontend.helpers_gui.global_styling import btn_action_style, label_desc_style
+from typing import Callable
 
 
 def display_list(
@@ -12,14 +13,29 @@ def display_list(
     page_size: int = 5,
     err_msg: str = "",
     place_row: int = 1,
+    place_col: int = 0,
     colspan: int = 2,
+    cmd: Callable = lambda: None,
 ) -> tuple[bool, ctk.CTkFrame]:
-    """Create a page with multiple options to choose from. The options are clustered into groups of 5 (by default)."""
+    """Create a page with multiple options to choose from. The options are clustered into groups of 5 (by default).
+
+    Parameters:
+        _master (tkinter.Tk|tkinter.Toplevel): The parent window.
+        options (tuple[str]): The options to be displayed.
+        returned_idx (list[ctk.IntVar]): variable to store the index of the selected option, the list is used to pass by reference. Just put a square bracket around the variable when passing it in.
+        selectable (bool): Whether the options are selectable or not.
+        page_size (int): The number of options to be displayed in a page.
+        err_msg (str): The error message to be displayed when there is no option to be displayed.
+        place_row (int): The row to place the list in the subwidget, this should be 1 because the first row is reserved for the page indicator.
+        place_col (int): The column to place the list in the subwidget, useful if you want to place multiple lists in the same subwidget.
+        colspan: depends on the master widget. If the design has x columns, then colspan should be x.
+        cmd (Callable): The command to be executed when an option is selected.
+    """
     clustered = clustering(options, page_size)
     current_page = 0
     total_page = len(clustered)
     empl_page = ctk.CTkFrame(master=_master)
-    empl_page.grid(row=place_row, column=0, columnspan=colspan, padx=20, pady=10)
+    empl_page.grid(row=place_row, column=place_col, columnspan=colspan, padx=20, pady=10)
     if total_page == 0:
         ctk.CTkLabel(master=empl_page, text=err_msg, **label_desc_style).grid(row=2, column=0, padx=20, pady=20)
         return False, empl_page
@@ -35,7 +51,7 @@ def display_list(
         for i, option in enumerate(clustered[current_page]):
             if selectable:
                 value = i + current_page * page_size
-                ctk.CTkRadioButton(master=empl_page, text=option, variable=returned_idx[0], value=value).grid(
+                ctk.CTkRadioButton(master=empl_page, text=option, variable=returned_idx[0], value=value, command=cmd).grid(
                     row=i + 1, column=0, padx=20, columnspan=2, sticky=tk.W
                 )
             else:
