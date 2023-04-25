3c3f804958dfee8a47bb5797474cc28a64502f2e
Delnegend <kiennguyen19323@gmail.com>
2023-04-25 21:48:31 +0700

    refactor(helpers GUI): cleanup init

--- frontend/helpers_gui/__init__.py
+++ frontend/helpers_gui/__init__.py
@@ -3,19 +3,4 @@ from .merge_callable import merge_callable
 from .display_list import display_list
 from .create_menu_buttons import MenuButtons
 
-__all__ = [
-    "merge_callable",
-    "display_list",
-    "MenuButtons",
-    # "btn_action_style",
-    # "btn_cancel_style",
-    # "btn_default_style",
-    # "btn_danger_style",
-    # "btn_info_style",
-    # "btn_primary_style",
-    # "btn_success_style",
-    # "btn_warning_style",
-    # "label_desc_style",
-    # "label_title_style",
-    # "label_warning_style",
-]
+__all__ = ["merge_callable", "display_list", "MenuButtons"]
