c5be7034435638e7ccb5450909a9acbea1186010
Delnegend <kiennguyen19323@gmail.com>
2023-04-27 18:59:07 +0700

    refactor(Empl): add incorrent phone length to err msg

--- models/employee.py
+++ models/employee.py
@@ -73,7 +73,7 @@ class Employee(BaseModel):
         if any(char.isalpha() for char in phone):
             return Err("Phone number cannot contain letters!")
         if len(phone) != 10:
-            return Err("Phone number must be 10 digits long!")
+            return Err(f"Phone number must be 10 digits long, not {len(phone)}!")
         self.phone = phone
         return Ok(self) if phone else Err("Phone number cannot be empty!")
 
